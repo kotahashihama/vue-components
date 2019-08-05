b61faa728f3c235fcc67e8115a1ccdbfcd7d3d7a
Kota Hashihama <kotahashihama@gmail.com>
2019-08-05 20:54:49 +0900

    カルーセルのStoryを追加

---
+++ src/components/carousel/Carousel.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="carousel">carousel</div>
+</template>
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,33 +1,8 @@
-/* eslint-disable react/react-in-jsx-scope */
+import { storiesOf } from "@storybook/vue";
 
-import { storiesOf } from '@storybook/vue';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import Carousel from "../src/components/carousel/Carousel.vue";
 
-import MyButton from './MyButton';
-import Welcome from './Welcome';
-
-storiesOf('Welcome', module).add('to Storybook', () => ({
-  components: { Welcome },
-  template: '<welcome :showApp="action" />',
-  methods: { action: linkTo('Button') },
+storiesOf("カルーセル", module).add("Carousel", () => ({
+  components: { Carousel },
+  template: "<Carousel />"
 }));
-
-storiesOf('Button', module)
-  .add('with text', () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">Hello Button</my-button>',
-    methods: { action: action('clicked') },
-  }))
-  .add('with JSX', () => ({
-    components: { MyButton },
-    render(h) {
-      return <my-button onClick={this.action}>With JSX</my-button>;
-    },
-    methods: { action: linkTo('clicked') },
-  }))
-  .add('with some emoji', () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
-    methods: { action: action('clicked') },
-  }));
