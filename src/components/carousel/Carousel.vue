769d65a13d96e1b27fcdba66c981eabdd229e2aa
Kota Hashihama <kotahashihama@gmail.com>
2019-08-06 10:44:12 +0900

    カルーセルの画像を配置

--- src/components/carousel/Carousel.vue
+++ src/components/carousel/Carousel.vue
@@ -1,3 +1,63 @@
 <template>
-  <div class="carousel">carousel</div>
+  <div class="carousel">
+    <ul class="carousel-list">
+      <CarouselItem
+        v-for="(item, index) in items"
+        :key="index"
+        :image="item.image"
+        :link="item.link"
+      />
+    </ul>
+  </div>
 </template>
+
+<script>
+import CarouselItem from "./CarouselItem.vue";
+
+export default {
+  components: {
+    CarouselItem
+  },
+  data() {
+    return {
+      items: [
+        {
+          image: "http://placehold.jp/800x450.png",
+          link: "a"
+        },
+        {
+          image: "http://placehold.jp/800x450.png",
+          link: ""
+        },
+        {
+          image: "http://placehold.jp/800x450.png",
+          link: ""
+        },
+        {
+          image: "http://placehold.jp/800x450.png",
+          link: ""
+        },
+        {
+          image: "http://placehold.jp/800x450.png",
+          link: ""
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.carousel {
+  overflow: hidden;
+  width: 800px;
+  height: 450px;
+}
+
+.carousel-list {
+  list-style: none;
+  display: flex;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/carousel/CarouselItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <li v-if="link" class="carousel-item">
+    <a :href="link">
+      <img :src="image" alt="" class="image" />
+    </a>
+  </li>
+  <li v-else class="carousel-item">
+    <img :src="image" alt="" class="image" />
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+      default: ""
+    },
+    link: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style>
+.image {
+  display: block;
+}
+</style>
